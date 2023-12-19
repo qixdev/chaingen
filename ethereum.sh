b336fc3953c19551c14c40e6faccc9a714c47b66
Danial <qqteam8@gmail.com>
2023-12-19 19:25:45 +0600

    security issues fix

--- ethereum.sh
+++ ethereum.sh
@@ -1,101 +1,50 @@
 #!/bin/bash
 
-display_error_and_exit() {
-    echo "Error: $1"
-    exit 1
+# Define the Geth download URL
+GETH_LINUX_URL="https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.17-25c9b49f.tar.gz"
+GETH_MAC_URL="https://gethstore.blob.core.windows.net/builds/geth-darwin-amd64-1.10.17-25c9b49f.tar.gz"
+
+# Function to install Geth for Linux
+install_geth_linux() {
+    echo "Downloading Geth for Linux..."
+    curl -O $GETH_LINUX_URL
+    tar -xvzf geth-linux-amd64-*.tar.gz
+    mv geth-linux-amd64-*/geth /usr/local/bin
+    rm -rf geth-linux-amd64-*
+    echo "Geth installation completed."
 }
 
-# Check if the operating system is Linux
-if [[ "$(uname)" == "Linux" ]]; then
-    echo "[ChainGen] Deploying an Ethereum node on Linux."
-    ETHEREUM_INSTALL_DIR="$HOME/ethereum"
+# Function to install Geth for macOS
+install_geth_mac() {
+    echo "Downloading Geth for macOS..."
+    curl -O $GETH_MAC_URL
+    tar -xvzf geth-darwin-amd64-*.tar.gz
+    mv geth-darwin-amd64-*/geth /usr/local/bin
+    rm -rf geth-darwin-amd64-*
+    echo "Geth installation completed."
+}
 
-    mkdir -p "$ETHEREUM_INSTALL_DIR"
+# Check the OS and install Geth accordingly
+OS="$(uname)"
+case $OS in
+  "Linux")
+    install_geth_linux
+    ;;
+  "Darwin")
+    install_geth_mac
+    ;;
+  *)
+    echo "Unsupported operating system: $OS"
+    exit 1
+    ;;
+esac
 
-    ETHEREUM_CORE_URL="https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-<ethereum-core-version>.tar.gz"
-    ETHEREUM_CORE_TAR="$ETHEREUM_INSTALL_DIR/geth.tar.gz"
+# Initialize and run the Ethereum node
+echo "Initializing Ethereum node..."
+geth --datadir ~/.ethereum/mainnet init
 
-    echo "Downloading Geth..."
-    curl -o "$ETHEREUM_CORE_TAR" "$ETHEREUM_CORE_URL"
+echo "Starting Ethereum node on Mainnet..."
+geth --datadir ~/.ethereum/mainnet
 
-    echo "Extracting Geth..."
-    tar -xzf "$ETHEREUM_CORE_TAR" -C "$ETHEREUM_INSTALL_DIR" --strip-components=1
-    rm "$ETHEREUM_CORE_TAR"
-
-    ETHEREUM_DATA_DIR="$HOME/ethereum-data"
-    mkdir -p "$ETHEREUM_DATA_DIR"
-    ETHEREUM_CONF_FILE="$ETHEREUM_DATA_DIR/geth.conf"
-    echo "server=1" > "$ETHEREUM_CONF_FILE"
-    echo "rpcuser=<yourusername>" >> "$ETHEREUM_CONF_FILE"
-    echo "rpcpassword=<yourpassword>" >> "$ETHEREUM_CONF_FILE"
-    "$ETHEREUM_INSTALL_DIR/geth" --datadir="$ETHEREUM_DATA_DIR" --syncmode=full
-    read -p "Press Enter to exit..."
-    exit 0
-# Check if the operating system is Windows
-elif [[ "$(uname)" == "MINGW"* || "$(uname)" == "CYGWIN"* ]]; then
-    # Ethereum installation directory
-    ETHEREUM_INSTALL_DIR="C:/Ethereum"
-
-    # Create Ethereum installation directory if it doesn't exist
-    mkdir -p "$ETHEREUM_INSTALL_DIR"
-
-    # Download and install Geth
-    ETHEREUM_CORE_URL="https://gethstore.blob.core.windows.net/builds/geth-windows-amd64-<ethereum-core-version>.zip"
-    ETHEREUM_CORE_ZIP="$ETHEREUM_INSTALL_DIR/geth.zip"
-
-    echo "Downloading Geth..."
-    curl -o "$ETHEREUM_CORE_ZIP" "$ETHEREUM_CORE_URL"
-
-    echo "Extracting Geth..."
-    unzip -q "$ETHEREUM_CORE_ZIP" -d "$ETHEREUM_INSTALL_DIR"
-    rm "$ETHEREUM_CORE_ZIP"
-
-    # Create Ethereum data directory
-    ETHEREUM_DATA_DIR="C:/EthereumData"
-    mkdir -p "$ETHEREUM_DATA_DIR"
-
-    # Create Ethereum configuration file
-    ETHEREUM_CONF_FILE="$ETHEREUM_DATA_DIR/geth.conf"
-    echo "server=1" > "$ETHEREUM_CONF_FILE"
-    echo "rpcuser=<yourusername>" >> "$ETHEREUM_CONF_FILE"
-    echo "rpcpassword=<yourpassword>" >> "$ETHEREUM_CONF_FILE"
-
-    # Start Geth
-    "C:\Ethereum\geth.exe" --datadir="$ETHEREUM_DATA_DIR" --syncmode=full
-
-    # Keep the terminal open
-    read -p "Press Enter to exit..."
-elif [[ "$(uname)" == "Darwin" ]]; then
-    echo "[ChainGen] Deploying an Ethereum node on macOS."
-    ETHEREUM_INSTALL_DIR="$HOME/ethereum"
-
-    # Create installation directory
-    mkdir -p "$ETHEREUM_INSTALL_DIR" || display_error_and_exit "Failed to create installation directory."
-
-    # Download Geth tarball (replace the URL with the actual tarball URL)
-    ETHEREUM_CORE_URL="https://gethstore.blob.core.windows.net/builds/geth-darwin-amd64-<ethereum-core-version>.tar.gz"
-    ETHEREUM_CORE_TAR="$ETHEREUM_INSTALL_DIR/geth.tar.gz"
-    echo "Downloading Geth..."
-    curl -o "$ETHEREUM_CORE_TAR" "$ETHEREUM_CORE_URL" || display_error_and_exit "Failed to download Geth."
-
-    echo "Extracting Geth..."
-    tar -xzf "$ETHEREUM_CORE_TAR" -C "$ETHEREUM_INSTALL_DIR" --strip-components=1 || display_error_and_exit "Failed to extract Geth."
-    rm "$ETHEREUM_CORE_TAR"
-
-    ETHEREUM_DATA_DIR="$HOME/ethereum-data"
-    mkdir -p "$ETHEREUM_DATA_DIR" || display_error_and_exit "Failed to create data directory."
-
-    ETHEREUM_CONF_FILE="$ETHEREUM_DATA_DIR/geth.conf"
-    echo "server=1" > "$ETHEREUM_CONF_FILE"
-    echo "rpcuser=<yourusername>" >> "$ETHEREUM_CONF_FILE"
-    echo "rpcpassword=<yourpassword>" >> "$ETHEREUM_CONF_FILE"
-
-    "$ETHEREUM_INSTALL_DIR/geth" --datadir="$ETHEREUM_DATA_DIR" --syncmode=full || display_error_and_exit "Failed to start Geth."
-
-    echo "Ethereum node deployed successfully."
-    read -p "Press Enter to exit..."
-    exit 0
-else
-    echo "Unsupported operating system."
-fi
-exit 1
+# The script ends here. The node will continue to run in the foreground.
+# Use Ctrl+C to stop the node.
--- helpers/pipinstall.py
+++ helpers/pipinstall.py
@@ -1,6 +1,9 @@
 from openai import OpenAI
+from config import API_KEY
+client = OpenAI(api_key=API_KEY)
 
-client = OpenAI(api_key="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")
+
+
 
 # response = client.chat.completions.with_raw_response.create(
 #     messages=[{
@@ -10,15 +13,29 @@ client = OpenAI(api_key="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")
 #     model="gpt-3.5-turbo",
 # )
 
+# params = {
+#     'model': 'text-davinci-003',
+#     'temperature': 0.8,           
+#     'max_tokens': 100,            
+#     'seed': 42,                   
+#     'top_p': 0.7                  
+# }
+
 def generate(text):
     #script = ""
     #initial = f"""I want you to generate bash script out of this documentation: {text}. Provide installing all tools if they're not installed on computer if it's linux or mac, or provide error message if it's windows or other. If documentation has no commands to include in bash script, configure this script to be exact as documentation describes: {script}"""
     response = client.chat.completions.with_raw_response.create(
     messages=[{
-        "role": "user",
-        "content": text,
-    }],
+        "role": "system", 
+        "content": "You generate single bash script for blockchain nodes deployment without printing other information because it will be easier for me to parse your response if there is no other information besides script."
+        }, 
+#"You are AI that stands behind service called \"ChainGen\" that generates bash scripts for blockchain nodes deployment. Your scripts based on user's prompt and his desires, but you always should generate a single bash script. Your bash script should work on Windows, Linux and MacOS. You should download tools for deploying node using curl or wget and use latest version of tools. You should provide only script, no any instruction how to run it, just script, nothing else. User can provide documentation that can have bash commands directly and can have undirected operations that should be made with bash like \" create folder called X\" and you have to assemble that documentation into bash script.
+        {"role": "user", 
+        "content": f"{text}. Your scripts based on user's prompt and his desires, but you always should generate a single bash script. Your bash script should work on Windows, Linux and MacOS. You should download tools for deploying node using curl or wget and use latest version of tools. You should provide only script, no any instruction how to run it, just script, nothing else. User can provide documentation that can have bash commands directly and can have undirected operations that should be made with bash like \" create folder called X\" and you have to assemble that documentation into bash script."}],
     model="gpt-4-1106-preview",
+    temperature=0.1,
+    seed=42,
+    top_p=0.1
     )
     completion = response.parse()     # get the object that `chat.completions.create()` would have returned
     otvet = completion.choices[0].message.content
