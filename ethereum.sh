3a893a577730e2710163d72ae85b9a79b8c63187
Ansar <ansarsh1243@gmail.com>
2023-12-19 16:57:04 +0600

    Ethereum lil fix

--- ethereum.sh
+++ ethereum.sh
@@ -11,6 +11,7 @@ mkdir -p "$ETH_INSTALL_DIR"
 
 # Create Prysm installation directory if it doesn't exist
 mkdir -p "$PRYSM_INSTALL_DIR"
+PRYSM_URL="https://github.com/prysmaticlabs/prysm/archive/refs/tags/v4.1.1.zip"
 
 # Download and install Geth
 echo "Downloading Geth..."
@@ -20,9 +21,9 @@ rm "$ETH_INSTALL_DIR/geth.zip"
 
 # Download and install Prysm
 echo "Downloading Prysm..."
-curl -o "$PRYSM_INSTALL_DIR/prysm.zip" "https://github.com/prysmaticlabs/prysm/archive/refs/tags/v4.1.1.zip"
-unzip -q "$PRYSM_INSTALL_DIR/prysm.zip" -d "$PRYSM_INSTALL_DIR"
-rm "$PRYSM_INSTALL_DIR/prysm.zip"
+curl -o "$PRYSM_INSTALL_DIR/prysm.zip" "PRYSM_URL"
+unzip -q "$PRYSM_INSTALL_DIR/prysm-4.1.1.zip" -d "$PRYSM_INSTALL_DIR"
+rm "$PRYSM_INSTALL_DIR/prysm-4.1.1.zip"
 
 # Initialize Prysm (replace wallet and validator information)
 echo "Initializing Prysm..."
@@ -30,7 +31,7 @@ echo "Initializing Prysm..."
 
 # Start Geth (replace with your desired Geth configuration)
 echo "Starting Geth..."
-"$ETH_INSTALL_DIR/geth.exe" --syncmode "snap" --cache=1024
+"C:\Ethereum\geth-windows-amd64-1.13.5-916d6a44/geth.exe" --syncmode "snap" --cache=1024
 
 # Keep the terminal open
 read -p "Press Enter to exit..."
